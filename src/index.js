35e8ae8ac362f2df531394ac48ba9aef4afa6ef5
Yuvarajgitcat <yuvarajr0326@gmail.com>
2025-07-20 17:06:33 +0530

    connected to MongoAtlas

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require("dotenv").config();
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: "./env"
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+// ( async () => {
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (err) => {
+//             console.error("Express server error:", err);
+//             throw err;
+//         });
+//         app.listen(process.env.PORT, () => {
+//             console.log(`Server is running on port ${process.env.PORT}`);
+//         })
+//     }
+//     catch(error){
+//         console.error("Error connecting to MongoDB:", error);
+//         throw error;
+//     }
+// })()
